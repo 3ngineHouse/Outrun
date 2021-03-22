f71d24e7c92ffa275b670794068816549c64452b
3ngineHouse <james.e.house@hotmail.co.uk>
2021-03-22 18:34:15 +0000

    truck added, with steering

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 const gameFrame = document.querySelector("#gameFrame");
 const obstacles = document.querySelectorAll(".obstacle");
-let speed = 5;
+const truck = document.querySelector("#truck");
+let speed = 8;
+let steer = 0;
 
 console.log(gameFrame.offsetHeight);
 obstacles.forEach(obstacle => {
     console.log("check");
     console.log(obstacle.offsetTop)
+});
+
+document.addEventListener("keydown", function startAction (event) {
+    if (event.key === "a") {
+        console.log("start turning left!");
+        steer = -1;
+    }
+
+    if (event.key === "d") {
+        console.log("start turning right!");
+        steer = 1;
+    }
+});
+
+document.addEventListener("keyup", function stopAction (event) {
+    if (event.key === "a") {
+        // We straighten only if the player hasn't already started turning right
+        if (steer == -1){
+            steer = 0;
+        }
+    }
+
+    if (event.key === "d") {
+        // We straighten only if the player hasn't already started turning left
+        if (steer == 1){
+            steer = 0;
+        }
+    }
 });
 
 const startGame = () => {
@@ -20,10 +50,10 @@ const startGame = () => {
     window.requestAnimationFrame(gameLoop);
 }
 
-function gameLoop () {
+const gameLoop = () => {
     moveObstacle();
+    moveTruck();
 //  Check for collison
-//  Move vehicle
 //  Move drones?
 //  Check for drone collision?    
     window.requestAnimationFrame(gameLoop);
@@ -32,12 +62,23 @@ function gameLoop () {
 const moveObstacle = () => {
     obstacles.forEach(obstacle => {
         if(obstacle.offsetTop>gameFrame.offsetHeight){
-            obstacle.style.top="-200px";
+            obstacle.style.top=`-${obstacle.offsetHeight+50}px`;
             setObstacle(obstacle);
         } else {
             obstacle.style.top=`${obstacle.offsetTop+speed}px`;
         }
     });
+}
+
+const moveTruck = () => {
+    let newPosition = truck.offsetLeft+(steer*5);
+
+    if(newPosition > (gameFrame.offsetWidth-truck.offsetWidth)){
+        newPosition = (gameFrame.offsetWidth-truck.offsetWidth);
+    } else if (newPosition < 0) {
+        newPosition = 0;
+    }
+    truck.style.left=`${newPosition}px`
 }
 
 const setObstacle = (obstacle) => {
